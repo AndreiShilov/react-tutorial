18868724ae0b2f7d6484200251d7d9f7892ee408
Andrei Shilov <andrei_shilov@epam.com>
2016-09-07 19:09:48 +0300

    Lesson 7 Using refs

--- app/App.jsx
+++ app/App.jsx
@@ -6,31 +6,49 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            text: 'this is the state'
+            red: 0,
+            green: 0,
+            blue: 0
         };
         this.update = this.update.bind(this)
     }
 
     update(e) {
         this.setState({
-            text: e.target.value
+            red: ReactDom.findDOMNode(this.refs.red).value,
+            green: ReactDom.findDOMNode(this.refs.green).value,
+            blue: ReactDom.findDOMNode(this.refs.blue).value
         })
     }
 
     render() {
-        return <Widget text={this.state.text} update={this.update}/>
+        return (
+            <div>
+                <div>
+                    red : {this.state.red}
+                    <Slider ref="red" update={this.update}/>
+                </div>
+                <div>
+                    green: {this.state.green}
+                    <Slider ref="green" update={this.update}/>
+                </div>
+                <div>
+                    blue: {this.state.blue}
+                    <Slider ref="blue" update={this.update}/>
+                </div>
+            </div>
+        )
     }
 }
 
-
-const Widget = (props) => {
-    return <div>
-        <h1>Hello world</h1>
-        <input type="text" onChange={props.update}/>
-        <div>{props.text}</div>
-    </div>
-
+class Slider extends Component {
+    render() {
+        return (
+            <input type="range" min="0" max="250" onChange={this.props.update}/>
+        )
+    }
 }
+
 
 ReactDom.render(
     <App />,
