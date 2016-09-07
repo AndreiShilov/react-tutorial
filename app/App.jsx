36335c8d00fb6ef64f725ed02c24ae0b0c71cdf7
Andrei Shilov <andrei_shilov@epam.com>
2016-09-07 18:57:36 +0300

    Lesson 6 Owner Ownee RelationShip

--- app/App.jsx
+++ app/App.jsx
@@ -3,28 +3,34 @@ import ReactDom from 'react-dom';
 
 
 class App extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             text: 'this is the state'
-        }
+        };
+        this.update = this.update.bind(this)
     }
 
-    update(e){
+    update(e) {
         this.setState({
             text: e.target.value
         })
     }
 
     render() {
-        return <div>
-            <h1>Hello world</h1>
-            <input type="text" onChange={this.update.bind(this)}/>
-            <div>{this.state.text}</div>
-        </div>
+        return <Widget text={this.state.text} update={this.update}/>
     }
 }
 
+
+const Widget = (props) => {
+    return <div>
+        <h1>Hello world</h1>
+        <input type="text" onChange={props.update}/>
+        <div>{props.text}</div>
+    </div>
+
+}
 
 ReactDom.render(
     <App />,
