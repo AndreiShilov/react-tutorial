fc78b802a80e21dc7c742ce67bd9ffa4d83d26fc
Andrei Shilov <andrei_shilov@epam.com>
2016-09-07 18:46:26 +0300

    Lesson 4

--- app/App.jsx
+++ app/App.jsx
@@ -3,20 +3,25 @@ import ReactDom from 'react-dom';
 
 
 class App extends Component {
-    render () {
-        const text =this.props.text;
+    render() {
+        const text = this.props.text;
+        const cat = this.props.cat;
         return <div>
             <h1>Hello world</h1>
             <div>{text}</div>
-            </div>
+            <div>{cat}</div>
+        </div>
     }
 }
 
 App.propTypes = {
     text: PropTypes.string.isRequired,
     cat: PropTypes.number
-}
+};
 
+App.defaultProps = {
+    cat: 10
+};
 // const App = () => <h1>Hello from const</h1>
 
 export default App
@@ -25,5 +30,5 @@ export default App
 ReactDom.render(
     <App text="Passed text 2"/>,
     document.getElementById('app')
-)
+);
 
