e14d367e8153d97a2aaa06e2a2f1d0da654bce19
Andrei Shilov <andrei_shilov@epam.com>
2016-09-08 12:01:29 +0300

    Lesson15 Live Jsx Compiler

--- app/App.jsx
+++ app/App.jsx
@@ -6,44 +6,47 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            data: [
-                {id: 1, name: "Name" + 1},
-                {id: 2, name: "Name" + 2},
-                {id: 3, name: "Name" + 3},
-                {id: 4, name: "Name" + 4},
-                {id: 5, name: "Name" + 5},
-                {id: 6, name: "Name" + 6},
-                {id: 7, name: "Name" + 7},
-                {id: 8, name: "Name" + 8},
-                {id: 9, name: "Name" + 9},
-                {id: 10, name: "Name" + 10},
-                {id: 11, name: "Name" + 11},
-                {id: 12, name: "Name" + 12},
-                {id: 13, name: "Name" + 13},
-                {id: 14, name: "Name" + 14},
-                {id: 15, name: "Name" + 15},
-            ]
+            input: '/* add your jsx here*/',
+            output: '',
+            error: '',
         };
-
+        this.update = this.update.bind(this);
     }
 
+
+    update(e) {
+
+        let code = e.target.value;
+
+        try {
+            this.setState({
+                output: babel.transform(code, {
+                    stage: 0,
+                    loose: 'all'
+                }).code
+            })
+        } catch (e) {
+            this.setState({
+                error: e.message
+            })
+        }
+    }
 
     render() {
-        let rows = this.state.data.map(person => {
-            return <PersonRow key={person.id} data={person}/>
-        });
-        return <table>
-            <tbody>{rows}</tbody>
-        </table>
+        return <div>
+            <header>{this.state.error}</header>
+            <div className="container">
+                <textarea name="" id="" cols="30" rows="10"
+                          onChange={this.update}
+                          defaultValue={this.state.input}></textarea>
+                <pre>
+                    {this.state.output}
+
+                </pre>
+            </div>
+        </div>
     }
 }
-
-const PersonRow = (props) => {
-    return <tr>
-        <td>{props.data.id}</td>
-        <td>{props.data.name}</td>
-    </tr>
-};
 
 ReactDom.render(
     <App />,
